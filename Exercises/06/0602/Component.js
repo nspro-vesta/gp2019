f0d6877f5d8365b76a6d71523a3788ea18f985b8
arshad feeroz <arshadfeeroz2012@gmail.com>
2019-01-30 15:09:15 +0800

    Create Component.js

---
+++ Exercises/06/0602/Component.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel) {
+	"use strict";
+
+	return UIComponent.extend("YourNamespace.Component", {
+
+		metadata: {
+			rootView: {
+				"viewName": "YourNameSpace.view.App",
+				"type": "XML",
+				"async": true,
+				"id": "app"
+			}
+		},
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+		}
+	});
+});
